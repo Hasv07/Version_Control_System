63b66b6c2686826558bed85dbb672648ceb80659
Hassan Khalifa <48550975+Hasv07@users.noreply.github.com>
2023-11-01 11:15:42 +0200

    MIN: Add Main:getMin()

--- main.cpp
+++ main.cpp
@@ -9,8 +9,26 @@ int getSum(const std::vector<int>& vec) {
     return sum;
 }
 
+int getMin(const std::vector<int>& vec) {
+    int min = vec[0];
+    for (int i = 1; i < vec.size(); ++i) {
+        if (vec[i] < min) {
+            min = vec[i];
+        }
+    }
+    return min;
+}
+
 int main(){
+
     std::vector<int> numbers = {1, 2, 3};
+    
     int sum = getSum(numbers);
     std::cout << "Sum: " << sum << std::endl;
+
+
+    int min = getMin(numbers);
+    std::cout << "Minimum: " << min << std::endl;
+
+
 }
