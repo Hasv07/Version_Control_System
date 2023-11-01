38240b9a67291431648b9de86aff0c2b327812e5
Hassan Khalifa <48550975+Hasv07@users.noreply.github.com>
2023-11-01 11:08:40 +0200

    AVG: Add Main:getAvg()

--- main.cpp
+++ main.cpp
@@ -9,8 +9,22 @@ int getSum(const std::vector<int>& vec) {
     return sum;
 }
 
+double getAverage(const std::vector<int>& vec) {
+    double sum = getSum(vec);
+    double average = sum / vec.size();
+    return average;
+}
+
+
 int main(){
+
     std::vector<int> numbers = {1, 2, 3};
+    
     int sum = getSum(numbers);
     std::cout << "Sum: " << sum << std::endl;
+
+    double average = getAverage(numbers);
+    std::cout << "Average: " << average << std::endl;
+
+
 }
